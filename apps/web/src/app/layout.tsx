86ed32dfaa4ef78defd7d2b49527a997c7f9240b
Debashish Patra <suvam0451@gmail.com>
2024-05-12 17:26:28 +0530

    chore: use different lib for seo

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,6 +8,8 @@ import {
 } from '@tanstack/react-query';
 import {createTheme, MantineProvider} from '@mantine/core';
 import {Helmet} from 'react-helmet';
+import { NextSeo } from 'next-seo';
+
 
 import './globals.css';
 import StyledComponentsRegistry from '../lib/registry';
@@ -30,12 +32,38 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+		<NextSeo
+			title="Yin | Chatbot and Prompt Assistant"
+			description="Create and interact with multiple chatbot personas ◆ Generate AI images using Dall-E and Stable Diffusion ◆ Customise your assistant with unique personality."
+			openGraph={{
+				url: 'https://avatars.githubusercontent.com/u/44526763?v=4',
+				title: 'Yin | Chatbot and Prompt Assistant',
+				description: 'Create and interact with multiple chatbot personas ◆ Generate AI images using Dall-E and Stable Diffusion ◆ Customise your assistant with unique personality.',
+				images: [
+					{
+						url: 'https://avatars.githubusercontent.com/u/44526763?v=4',
+						width: 800,
+						height: 600,
+						alt: 'Og Image Alt',
+						type: 'image/jpeg',
+					},
+					{
+						url: 'https://avatars.githubusercontent.com/u/44526763?v=4',
+						width: 900,
+						height: 800,
+						alt: 'Og Image Alt Second',
+						type: 'image/jpeg',
+					},
+				],
+				siteName: 'SiteName',
+			}}
+		/>
 		<Helmet>
 			<title>Yin | Chatbot and Prompt Assistant</title>
 			<meta property="og:title" content="Yin | Chatbot and Prompt Assistant"
 						key="title" />
 			<meta name="description"
-						content="Create and interact with multiple chatbot personas ◆ Generate AI images using Dall-E and OpenAI ◆ Customise your assistant with unique personality." />
+						content="Create and interact with multiple chatbot personas ◆ Generate AI images using Dall-E and Stable Diffusion ◆ Customise your assistant with unique personality." />
 			<meta property="og:description"
 						content="Create and interact with multiple chatbot personas ◆ Generate AI images using Dall-E and OpenAI ◆ Customise your assistant with unique personality." />
 			<meta property="og:image:url" content="https://avatars.githubusercontent.com/u/44526763?v=4" />
